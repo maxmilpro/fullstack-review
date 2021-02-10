b6eec4650873dec2136bec019c3ce5316958bf70
Max Proano <proanom89@gmail.com>
2021-02-10 15:13:07 -0800

    Complete linking repo name to github url

--- client/src/components/Repo.jsx
+++ client/src/components/Repo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Repo = ({repo}) => {
 return (
   <tr>
-    <td>{repo.name}</td>
+    <td><a href={repo.htmlUrl}>{repo.name}</a></td>
     <td>{repo.owner}</td>
     <td>{repo.forks}</td>
   </tr>
