2328aacdbb9c4d29a4598042e6851bb0a6238ccd
Max Proano <proanom89@gmail.com>
2021-02-12 17:36:22 -0800

    Updated mongoose and connected to atlas

--- database/index.js
+++ database/index.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
-mongoose.connect(`mongodb+srv://dbUser:${process.env.CLUSTER}@cluster0.i6urc.mongodb.net/fetcher?retryWrites=true&w=majority`);
+mongoose.connect(`mongodb+srv://dbUser:${process.env.CLUSTER}@cluster0.i6urc.mongodb.net/fetcher?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function() {
+  console.log(`we're connected`);
+})
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
